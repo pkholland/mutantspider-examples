5abf48dd019c6dc98dcbc63c43c5fe8b14dfa76b
Paul Holland <pholland@adobe.com>
2016-02-17 15:38:51 -0800

    moving to real mutantspider

--- web3d/scripts/npm_postInstall.sh
+++ web3d/scripts/npm_postInstall.sh
@@ -2,7 +2,7 @@
 
 node node_modules/npm-gitter/npm-gitter.js repo_url=https://github.com/assimp/assimp.git local_dir=../other_repos/assimp
 #node node_modules/npm-gitter/npm-gitter.js repo_url=https://github.com/pkholland/assimp.git local_dir=../other_repos/assimp
-node node_modules/npm-gitter/npm-gitter.js repo_url=https://github.com/pkholland/tools.git local_dir=../other_repos/mutantspider
+node node_modules/npm-gitter/npm-gitter.js repo_url=https://github.com/pkholland/mutantspider.git local_dir=../other_repos/mutantspider
 #node node_modules/npm-gitter/npm-gitter.js repo_url=https://github.com/acgessler/assimp2json.git local_dir=../other_repos/assimp2json
 node node_modules/npm-gitter/npm-gitter.js repo_url=https://github.com/pkholland/assimp2json.git local_dir=../other_repos/assimp2json
 
